90359a797cf870836d03e0e0fee45e7274e08539
Yasmin de Souza <yasminsbatista@hotmail.com>
2021-07-29 17:58:49 -0300

    adicionando pagina room

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import  "./styles/global.scss"
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
