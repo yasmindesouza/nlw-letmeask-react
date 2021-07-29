e2dd50b0b7c3010a572992382dbac492b0eb5466
Yasmin de Souza <yasminsbatista@hotmail.com>
2021-07-29 16:47:10 -0300

    aula 1: criando paginas home e newRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-    <h1>Oi</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
