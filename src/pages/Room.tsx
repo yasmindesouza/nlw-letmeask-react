c89d0d68f693601d13fe6c0c6bfe788b9fd74d18
Yasmin de Souza <yasminsbatista@hotmail.com>
2021-07-29 23:04:32 -0300

    adicionando adminRoom page e fazendo deploy

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -7,7 +7,7 @@ import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
-// { useRoom } from '../hooks/useRoom';
+import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
 import '../styles/room.scss';
